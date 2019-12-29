1cd6ebfc4901623cb64a043f57a460a46126a96d
ezio84 <brabus84@gmail.com>
2019-12-29 08:54:58 +0100

    Settings: Disable Google Play system update check

--- src/com/android/settings/deviceinfo/firmwareversion/MainlineModuleVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/MainlineModuleVersionPreferenceController.java
@@ -47,7 +47,7 @@ public class MainlineModuleVersionPreferenceController extends BasePreferenceCon
 
     @Override
     public int getAvailabilityStatus() {
-        return !TextUtils.isEmpty(mModuleVersion) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+        return /*!TextUtils.isEmpty(mModuleVersion) ? AVAILABLE : */UNSUPPORTED_ON_DEVICE;
     }
 
     private void initModules() {
