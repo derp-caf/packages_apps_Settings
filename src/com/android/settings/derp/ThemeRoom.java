9bbddeb5171422a4864ea40ce3f9dc4bd5a82cce
SuperDroidBond <superdroidbond@yahoo.com>
2020-07-09 20:42:49 +0530

    Settings: move Overlays Preference to ThemeRoom
    Change-Id: I212ce0cf763a16aed0c4e81fca965771eb3e9c2a

--- src/com/android/settings/derp/ThemeRoom.java
+++ src/com/android/settings/derp/ThemeRoom.java
@@ -21,6 +21,7 @@ import android.os.RemoteException;
 import android.os.SystemProperties;
 import android.os.ServiceManager;
 import android.os.UserHandle;
+import androidx.fragment.app.Fragment;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceManager;
@@ -35,16 +36,28 @@ import android.view.MenuInflater;
 import com.android.settings.R;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 
 import com.android.internal.util.derp.ThemesUtils;
 import com.android.internal.util.derp.DerpUtils;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.dashboard.DashboardFragment;
+import android.provider.SearchIndexableResource;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.Indexable;
+import com.android.settingslib.search.SearchIndexable;
+import com.android.settings.display.OverlayCategoryPreferenceController;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
-public class ThemeRoom extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+public class ThemeRoom extends DashboardFragment implements
+        OnPreferenceChangeListener, Indexable {
+
+    private static final String TAG = "X ThemeRoom";
 
     private static final String ACCENT_COLOR = "accent_color";
     private static final String ACCENT_COLOR_PROP = "persist.sys.theme.accentcolor";
@@ -60,10 +73,21 @@ public class ThemeRoom extends SettingsPreferenceFragment implements
     private ListPreference mThemeSwitch;
 
     @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.theme_room;
+    }
+
+    @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
-        addPreferencesFromResource(R.xml.theme_room);
+//        addPreferencesFromResource(R.xml.theme_room);
+
         mUiModeManager = getContext().getSystemService(UiModeManager.class);
 
         mOverlayService = IOverlayManager.Stub
@@ -72,6 +96,23 @@ public class ThemeRoom extends SettingsPreferenceFragment implements
         setupAccentPref();
         setupThemeSwitchPref();
         setHasOptionsMenu(true);
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, Lifecycle lifecycle, Fragment fragment) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.font"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.adaptive_icon_shape"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.icon_pack.android"));
+        return controllers;
     }
 
     @Override
@@ -293,4 +334,25 @@ public class ThemeRoom extends SettingsPreferenceFragment implements
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.DERPZONE;
     }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.theme_room;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+    };
 }
